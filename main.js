2f241f6266498c3b884229071e55351bc60f3af9
Trevis Liu <trevisliu@gmail.com>
2024-08-26 18:17:22 -0400

    Fix issue with async import failing in build

--- main.js
+++ main.js
@@ -1,294 +1,297 @@
-import RAPIER from "@dimforge/rapier2d";
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
 import { renderObj } from "./renderer";
+// import RAPIER from "@dimforge/rapier2d";
+import RAPIER from "https://cdn.skypack.dev/@dimforge/rapier2d-compat";
 
-const RAD2DEG = 180 / Math.PI;
-const DEG2RAD = Math.PI / 180;
+RAPIER.init().then(() => {
+  const RAD2DEG = 180 / Math.PI;
+  const DEG2RAD = Math.PI / 180;
 
-let angleCursor;
-let angleActual;
-let cursorDistance;
-let usePrismaticJoint = true;
+  let angleCursor;
+  let angleActual;
+  let cursorDistance;
+  let usePrismaticJoint = true;
 
-let eventQueue = new RAPIER.EventQueue(true);
-eventQueue.drainCollisionEvents((handle1, handle2, started) => {
-  /* Handle the collision event. */
-  console.log("Collision detected");
-});
+  let eventQueue = new RAPIER.EventQueue(true);
+  eventQueue.drainCollisionEvents((handle1, handle2, started) => {
+    /* Handle the collision event. */
+    console.log("Collision detected");
+  });
 
-eventQueue.drainContactForceEvents((event) => {
-  let handle1 = event.collider1(); // Handle of the first collider involved in the event.
-  let handle2 = event.collider2(); // Handle of the second collider involved in the event.
-  /* Handle the contact force event. */
-});
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 10;
-
-var vec = new THREE.Vector3(); // create once and reuse
-var pos = new THREE.Vector3(); // create once and reuse
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Physics Engine
-let world = new RAPIER.World({ x: 0.0, y: -9.81 });
-
-// Creating the ground
-let groundW = 100;
-let groundH = 1;
-let groundX = 0;
-let groundY = -3;
-
-const groundGeo = new THREE.BoxGeometry(groundW, groundH, 1);
-const groundMat = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const groundObj = new THREE.Mesh(groundGeo, groundMat);
-scene.add(groundObj);
-
-let groundRb = world.createRigidBody(
-  RAPIER.RigidBodyDesc.fixed()
-    .setTranslation(groundX, groundY)
-    .setCcdEnabled(true)
-);
-let groundCd = world.createCollider(
-  RAPIER.ColliderDesc.cuboid(groundW / 2.0, groundH / 2.0)
-    .setCollisionGroups(0x00020001)
-    .setRestitution(0)
-    .setRestitutionCombineRule(RAPIER.CoefficientCombineRule.Min)
-    .setActiveEvents(RAPIER.ActiveEvents.COLLISION_EVENTS),
-  groundRb
-);
-
-// Creating the player
-const playerW = 0.5;
-const playerH = 1;
-
-const playerGeo = new THREE.BoxGeometry(playerW, playerH, 1);
-const playerMat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const playerObj = new THREE.Mesh(playerGeo, playerMat);
-scene.add(playerObj);
-
-let playerRb = world.createRigidBody(
-  new RAPIER.RigidBodyDesc(RAPIER.RigidBodyType.Dynamic)
-    .setTranslation(0.0, 0.0)
-    .setCanSleep(true)
-    .setCcdEnabled(true)
-);
-playerRb.lockRotations(true, true);
-
-let playerCd = world.createCollider(
-  RAPIER.ColliderDesc.cuboid(playerW / 2.0, playerH / 2.0)
-    .setTranslation(0.0, 0.0)
-    .setRestitution(0)
-    .setRestitutionCombineRule(RAPIER.CoefficientCombineRule.Min)
-    .setCollisionGroups(0x00010002),
-  playerRb
-);
-
-// Creating the hammer
-const hammerS = 0.25;
-
-const hammerGeo = new THREE.BoxGeometry(hammerS, hammerS, 1);
-const hammerMat = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-const hammerObj = new THREE.Mesh(hammerGeo, hammerMat);
-scene.add(hammerObj);
-
-let hammerRb = world.createRigidBody(
-  new RAPIER.RigidBodyDesc(RAPIER.RigidBodyType.Dynamic)
-    .setTranslation(0.0, 0.0)
-    .setGravityScale(0)
-    .setCanSleep(true)
-    .setCcdEnabled(true)
-);
-let hammerCd = world.createCollider(
-  RAPIER.ColliderDesc.cuboid(hammerS / 2.0, hammerS / 2.0)
-    .setTranslation(0.0, 0.0)
-    .setRestitution(0)
-    .setRestitutionCombineRule(RAPIER.CoefficientCombineRule.Min)
-    .setCollisionGroups(0x00010002)
-    .setActiveEvents(RAPIER.ActiveEvents.COLLISION_EVENTS),
-  hammerRb
-);
-// hammerCd.setFriction(1000);
-
-// Creating the intermediate object
-let intermediateW = 1.75;
-let intermediateH = 0.125;
-let prismaticMinimumLimit = 0.25;
-
-const intermediateGeo = new THREE.BoxGeometry(
-  intermediateW,
-  intermediateH,
-  0.25
-);
-const intermediateMat = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const intermediateObj = new THREE.Mesh(intermediateGeo, intermediateMat);
-intermediateObj.position.z = 0.5;
-scene.add(intermediateObj);
-
-let intermediateRb = world.createRigidBody(
-  new RAPIER.RigidBodyDesc(RAPIER.RigidBodyType.Dynamic)
-    .setTranslation(0.0, 0.0)
-    .setGravityScale(0)
-    // .setAdditionalMass(500)
-    // .setAngvel(100)
-    .setCanSleep(true)
-    .setCcdEnabled(true)
-);
-
-let intermediateCd = world.createCollider(
-  RAPIER.ColliderDesc.cuboid(intermediateW / 2.0, intermediateH / 2.0)
-    .setCollisionGroups(0x00010000)
-    .setTranslation(0.0, 0),
-  intermediateRb
-);
-
-// Create a revolute joint
-let revoluteJoint = world.createImpulseJoint(
-  RAPIER.JointData.revolute(
-    { x: 0.0, y: 0.0 },
-    { x: -intermediateW / 2.0, y: 0.0 }
-  ),
-  playerRb,
-  intermediateRb,
-  true
-);
-revoluteJoint.configureMotorModel(RAPIER.MotorModel.ForceBased);
-
-let prismaticJoint;
-if (usePrismaticJoint) {
-  // Create the prismatic joint
-  let params = RAPIER.JointData.prismatic(
-    { x: 0.0, y: 0.0 },
-    { x: intermediateW / 2.0, y: 0.0 }, // Note: Setting the anchor to the 2nd body's origin will set it to that the intermediate body's center
-    { x: 1.0, y: 0.0 }
+  eventQueue.drainContactForceEvents((event) => {
+    let handle1 = event.collider1(); // Handle of the first collider involved in the event.
+    let handle2 = event.collider2(); // Handle of the second collider involved in the event.
+    /* Handle the contact force event. */
+  });
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
   );
-  params.limitsEnabled = true;
-  // 0 here represents the farthest it can go,
-  // intermediateW would mean that the hammer is allowed to move up to the player's origin itself
-  // But we take off a small length of it to avoid that to enforce a minimum reach.
-  params.limits = [0, intermediateW - prismaticMinimumLimit]; // Suggestion: Maybe replace hard-coded constant?
-  prismaticJoint = world.createImpulseJoint(
-    params,
-    hammerRb,
-    intermediateRb,
-    true
+  camera.position.z = 10;
+
+  var vec = new THREE.Vector3(); // create once and reuse
+  var pos = new THREE.Vector3(); // create once and reuse
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // Physics Engine
+  let world = new RAPIER.World({ x: 0.0, y: -9.81 });
+
+  // Creating the ground
+  let groundW = 100;
+  let groundH = 1;
+  let groundX = 0;
+  let groundY = -3;
+
+  const groundGeo = new THREE.BoxGeometry(groundW, groundH, 1);
+  const groundMat = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const groundObj = new THREE.Mesh(groundGeo, groundMat);
+  scene.add(groundObj);
+
+  let groundRb = world.createRigidBody(
+    RAPIER.RigidBodyDesc.fixed()
+      .setTranslation(groundX, groundY)
+      .setCcdEnabled(true)
   );
-  prismaticJoint.configureMotorModel(RAPIER.MotorModel.ForceBased);
-} else {
-  // Attach the hammer to the intermediate body
-  let fixedJoint = world.createImpulseJoint(
-    RAPIER.JointData.fixed(
+  let groundCd = world.createCollider(
+    RAPIER.ColliderDesc.cuboid(groundW / 2.0, groundH / 2.0)
+      .setCollisionGroups(0x00020001)
+      .setRestitution(0)
+      .setRestitutionCombineRule(RAPIER.CoefficientCombineRule.Min)
+      .setActiveEvents(RAPIER.ActiveEvents.COLLISION_EVENTS),
+    groundRb
+  );
+
+  // Creating the player
+  const playerW = 0.5;
+  const playerH = 1;
+
+  const playerGeo = new THREE.BoxGeometry(playerW, playerH, 1);
+  const playerMat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const playerObj = new THREE.Mesh(playerGeo, playerMat);
+  scene.add(playerObj);
+
+  let playerRb = world.createRigidBody(
+    new RAPIER.RigidBodyDesc(RAPIER.RigidBodyType.Dynamic)
+      .setTranslation(0.0, 0.0)
+      .setCanSleep(true)
+      .setCcdEnabled(true)
+  );
+  playerRb.lockRotations(true, true);
+
+  let playerCd = world.createCollider(
+    RAPIER.ColliderDesc.cuboid(playerW / 2.0, playerH / 2.0)
+      .setTranslation(0.0, 0.0)
+      .setRestitution(0)
+      .setRestitutionCombineRule(RAPIER.CoefficientCombineRule.Min)
+      .setCollisionGroups(0x00010002),
+    playerRb
+  );
+
+  // Creating the hammer
+  const hammerS = 0.25;
+
+  const hammerGeo = new THREE.BoxGeometry(hammerS, hammerS, 1);
+  const hammerMat = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  const hammerObj = new THREE.Mesh(hammerGeo, hammerMat);
+  scene.add(hammerObj);
+
+  let hammerRb = world.createRigidBody(
+    new RAPIER.RigidBodyDesc(RAPIER.RigidBodyType.Dynamic)
+      .setTranslation(0.0, 0.0)
+      .setGravityScale(0)
+      .setCanSleep(true)
+      .setCcdEnabled(true)
+  );
+  let hammerCd = world.createCollider(
+    RAPIER.ColliderDesc.cuboid(hammerS / 2.0, hammerS / 2.0)
+      .setTranslation(0.0, 0.0)
+      .setRestitution(0)
+      .setRestitutionCombineRule(RAPIER.CoefficientCombineRule.Min)
+      .setCollisionGroups(0x00010002)
+      .setActiveEvents(RAPIER.ActiveEvents.COLLISION_EVENTS),
+    hammerRb
+  );
+  // hammerCd.setFriction(1000);
+
+  // Creating the intermediate object
+  let intermediateW = 1.75;
+  let intermediateH = 0.125;
+  let prismaticMinimumLimit = 0.25;
+
+  const intermediateGeo = new THREE.BoxGeometry(
+    intermediateW,
+    intermediateH,
+    0.25
+  );
+  const intermediateMat = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+  const intermediateObj = new THREE.Mesh(intermediateGeo, intermediateMat);
+  intermediateObj.position.z = 0.5;
+  scene.add(intermediateObj);
+
+  let intermediateRb = world.createRigidBody(
+    new RAPIER.RigidBodyDesc(RAPIER.RigidBodyType.Dynamic)
+      .setTranslation(0.0, 0.0)
+      .setGravityScale(0)
+      // .setAdditionalMass(500)
+      // .setAngvel(100)
+      .setCanSleep(true)
+      .setCcdEnabled(true)
+  );
+
+  let intermediateCd = world.createCollider(
+    RAPIER.ColliderDesc.cuboid(intermediateW / 2.0, intermediateH / 2.0)
+      .setCollisionGroups(0x00010000)
+      .setTranslation(0.0, 0),
+    intermediateRb
+  );
+
+  // Create a revolute joint
+  let revoluteJoint = world.createImpulseJoint(
+    RAPIER.JointData.revolute(
       { x: 0.0, y: 0.0 },
-      0.0,
-      { x: -(intermediateW / 2.0), y: 0.0 },
-      0
+      { x: -intermediateW / 2.0, y: 0.0 }
     ),
+    playerRb,
     intermediateRb,
-    hammerRb,
     true
   );
-}
+  revoluteJoint.configureMotorModel(RAPIER.MotorModel.ForceBased);
 
-// Tweaking constraints
-
-// Note: Increasing the mass will make it more difficult to move the player
-playerCd.setMass(0.01);
-playerRb.setGravityScale(9);
-
-// Note: Increasing the mass makes revolutions slower but
-// improves the effect of springing up
-hammerCd.setMass(0.001);
-// hammerCd.setFriction(2500);
-
-// Note: Setting this mass too high will swing the whole player
-// while setting it too low will lower the quality of control
-intermediateCd.setMass(0.001);
-
-function animate() {
-  // Execute one step in the Physics world
-  world.step(eventQueue);
-
-  renderObj(playerRb, playerObj);
-  renderObj(hammerRb, hammerObj);
-  renderObj(groundRb, groundObj);
-  // renderObj(handleRb, handleObj);
-  renderObj(intermediateRb, intermediateObj);
-
-  angleActual = intermediateCd.rotation();
-  // intermediateRb.setRotation(angleCursor, true);
-  let angleError = angleCursor - angleActual;
-  while (angleError < -180 * DEG2RAD) angleError += 360 * DEG2RAD;
-  while (angleError > 180 * DEG2RAD) angleError -= 360 * DEG2RAD;
-  // intermediateRb.setAngvel(angleError * 15, true);
-
-  // revoluteJoint.configureMotorVelocity(angleError, 100000);
-  // console.log(angleCursor);
-  revoluteJoint.configureMotorPosition(angleCursor, 15.0, 0.1);
-  // intermediateRb.setRotation(angleCursor, true);
-
-  // console.log(cursorDistance);
+  let prismaticJoint;
   if (usePrismaticJoint) {
-    // Configure the prismatic joint to position the hammer
-
-    // 0 is the furthest point away from the player.
-    // This is why we apply the difference of the cursor's distance and the width of the intermediate body
-    // We must clamp the position of the hammer by ensuring it does not go up to the player's origin.
-    prismaticJoint.configureMotorPosition(
-      Math.min(
-        intermediateW - Math.min(cursorDistance, intermediateW),
-        intermediateW - prismaticMinimumLimit
-      ),
-      // 0,
-      50, // Note: If this value is too low, you will notice a spring effect when attempting to land on the hammer
-      1
+    // Create the prismatic joint
+    let params = RAPIER.JointData.prismatic(
+      { x: 0.0, y: 0.0 },
+      { x: intermediateW / 2.0, y: 0.0 }, // Note: Setting the anchor to the 2nd body's origin will set it to that the intermediate body's center
+      { x: 1.0, y: 0.0 }
     );
+    params.limitsEnabled = true;
+    // 0 here represents the farthest it can go,
+    // intermediateW would mean that the hammer is allowed to move up to the player's origin itself
+    // But we take off a small length of it to avoid that to enforce a minimum reach.
+    params.limits = [0, intermediateW - prismaticMinimumLimit]; // Suggestion: Maybe replace hard-coded constant?
+    prismaticJoint = world.createImpulseJoint(
+      params,
+      hammerRb,
+      intermediateRb,
+      true
+    );
+    prismaticJoint.configureMotorModel(RAPIER.MotorModel.ForceBased);
   } else {
-    // Directly set the anchor of the revolute joint to position the hammer
-    revoluteJoint.setAnchor2({
-      x: -Math.min(cursorDistance, intermediateW) / 2.0 + intermediateW / 4.0,
-      y: 0.0,
-    });
+    // Attach the hammer to the intermediate body
+    let fixedJoint = world.createImpulseJoint(
+      RAPIER.JointData.fixed(
+        { x: 0.0, y: 0.0 },
+        0.0,
+        { x: -(intermediateW / 2.0), y: 0.0 },
+        0
+      ),
+      intermediateRb,
+      hammerRb,
+      true
+    );
   }
 
-  const playerPos = playerRb.translation();
-  camera.position.x = playerPos.x;
-  camera.position.y = playerPos.y;
+  // Tweaking constraints
 
-  // Render
-  renderer.render(scene, camera);
-}
+  // Note: Increasing the mass will make it more difficult to move the player
+  playerCd.setMass(0.01);
+  playerRb.setGravityScale(9);
 
-if (WebGL.isWebGL2Available()) {
-  // Initiate function or other initializations here
-  renderer.setAnimationLoop(animate);
-} else {
-  const warning = WebGL.getWebGL2ErrorMessage();
-  document.getElementById("container").appendChild(warning);
-}
+  // Note: Increasing the mass makes revolutions slower but
+  // improves the effect of springing up
+  hammerCd.setMass(0.001);
+  // hammerCd.setFriction(2500);
 
-window.addEventListener("mousemove", (e) => {
-  vec.set(
-    (e.clientX / window.innerWidth) * 2 - 1,
-    -(e.clientY / window.innerHeight) * 2 + 1,
-    0.5
-  );
+  // Note: Setting this mass too high will swing the whole player
+  // while setting it too low will lower the quality of control
+  intermediateCd.setMass(0.001);
 
-  vec.unproject(camera);
-  vec.sub(camera.position).normalize();
-  var distance = -camera.position.z / vec.z;
-  pos.copy(camera.position).add(vec.multiplyScalar(distance));
+  function animate() {
+    // Execute one step in the Physics world
+    world.step(eventQueue);
 
-  const playerPos = playerRb.translation();
-  const hammerPos = hammerRb.translation();
+    renderObj(playerRb, playerObj);
+    renderObj(hammerRb, hammerObj);
+    renderObj(groundRb, groundObj);
+    // renderObj(handleRb, handleObj);
+    renderObj(intermediateRb, intermediateObj);
 
-  cursorDistance = Math.hypot(pos.y - playerPos.y, pos.x - playerPos.x);
-  angleCursor = Math.atan2(pos.y - playerPos.y, pos.x - playerPos.x);
+    angleActual = intermediateCd.rotation();
+    // intermediateRb.setRotation(angleCursor, true);
+    let angleError = angleCursor - angleActual;
+    while (angleError < -180 * DEG2RAD) angleError += 360 * DEG2RAD;
+    while (angleError > 180 * DEG2RAD) angleError -= 360 * DEG2RAD;
+    // intermediateRb.setAngvel(angleError * 15, true);
+
+    // revoluteJoint.configureMotorVelocity(angleError, 100000);
+    // console.log(angleCursor);
+    revoluteJoint.configureMotorPosition(angleCursor, 15.0, 0.1);
+    // intermediateRb.setRotation(angleCursor, true);
+
+    // console.log(cursorDistance);
+    if (usePrismaticJoint) {
+      // Configure the prismatic joint to position the hammer
+
+      // 0 is the furthest point away from the player.
+      // This is why we apply the difference of the cursor's distance and the width of the intermediate body
+      // We must clamp the position of the hammer by ensuring it does not go up to the player's origin.
+      prismaticJoint.configureMotorPosition(
+        Math.min(
+          intermediateW - Math.min(cursorDistance, intermediateW),
+          intermediateW - prismaticMinimumLimit
+        ),
+        // 0,
+        50, // Note: If this value is too low, you will notice a spring effect when attempting to land on the hammer
+        1
+      );
+    } else {
+      // Directly set the anchor of the revolute joint to position the hammer
+      revoluteJoint.setAnchor2({
+        x: -Math.min(cursorDistance, intermediateW) / 2.0 + intermediateW / 4.0,
+        y: 0.0,
+      });
+    }
+
+    const playerPos = playerRb.translation();
+    camera.position.x = playerPos.x;
+    camera.position.y = playerPos.y;
+
+    // Render
+    renderer.render(scene, camera);
+  }
+
+  if (WebGL.isWebGL2Available()) {
+    // Initiate function or other initializations here
+    renderer.setAnimationLoop(animate);
+  } else {
+    const warning = WebGL.getWebGL2ErrorMessage();
+    document.getElementById("container").appendChild(warning);
+  }
+
+  window.addEventListener("mousemove", (e) => {
+    vec.set(
+      (e.clientX / window.innerWidth) * 2 - 1,
+      -(e.clientY / window.innerHeight) * 2 + 1,
+      0.5
+    );
+
+    vec.unproject(camera);
+    vec.sub(camera.position).normalize();
+    var distance = -camera.position.z / vec.z;
+    pos.copy(camera.position).add(vec.multiplyScalar(distance));
+
+    const playerPos = playerRb.translation();
+    const hammerPos = hammerRb.translation();
+
+    cursorDistance = Math.hypot(pos.y - playerPos.y, pos.x - playerPos.x);
+    angleCursor = Math.atan2(pos.y - playerPos.y, pos.x - playerPos.x);
+  });
 });
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ import topLevelAwait from "vite-plugin-top-level-await";
 export default defineConfig({
   plugins: [wasm(), topLevelAwait()],
   build: {
+    outDir: "./docs",
+    emptyOutDir: true, // also necessary
     rollupOptions: {
       external: [],
+      plugins: [wasm()],
     },
+    minify: false,
   },
 });
